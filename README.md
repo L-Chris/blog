975497a1f306f52562084949a0ee6583f7e5676d
L-Chris <563303226@qq.com>
2019-04-14 13:38:11 +0800

    feat: 目录生成脚本

--- README.md
+++ README.md
@@ -1,20 +1,23 @@
+
 # Blog
 
 > 记录一些工作中总结的方案，以及一些问题的解决方法
 
 目录
 
-- 系列专题
-  - [代理和开发调试方案]()
-  - [代码反编译]()
-  - [图片处理]()
-  - [网页唤醒App]()
+
+- 基础设施
+  - [Docker](https://github.com/L-Chris/blog/blob/master/基础设施/Docker.md)
+  - [Gitlab](https://github.com/L-Chris/blog/blob/master/基础设施/Gitlab.md)
+  - [Jenkins](https://github.com/L-Chris/blog/blob/master/基础设施/Jenkins.md)
+  - [私有npm](https://github.com/L-Chris/blog/blob/master/基础设施/私有npm.md)
 
 - 工程杂谈
-  - [代码部署]()
+  - [代码部署](https://github.com/L-Chris/blog/blob/master/工程杂谈/代码部署.md)
 
-- 基础实施
-  - [Gitlab]()
-  - [Jenkins]()
-  - [Docker]()
-  - [私有npm]()
+- 系列专题
+  - [代理和开发调试](https://github.com/L-Chris/blog/blob/master/系列专题/代理和开发调试.md)
+  - [代码反编译](https://github.com/L-Chris/blog/blob/master/系列专题/代码反编译.md)
+  - [图片处理](https://github.com/L-Chris/blog/blob/master/系列专题/图片处理.md)
+  - [代码格式化方案](https://github.com/L-Chris/blog/blob/master/系列专题/代码格式化方案.md)
+  - [网页唤醒App](https://github.com/L-Chris/blog/blob/master/系列专题/网页唤醒App.md)
--- build.js
+++ build.js
@@ -1,7 +1,45 @@
+const fs = require('fs')
+const path = require('path')
+const fg = require('fast-glob')
+
+const linkPrefix = 'https://github.com/L-Chris/blog/blob/master/'
 const template = `
 # Blog
 
 > 记录一些工作中总结的方案，以及一些问题的解决方法
 
 目录
+
 `
+
+const stream = fg.stream('**/*.md', {
+  ignore: ['node_modules', 'README.md']
+})
+
+const entries = []
+
+stream.on('data', entry => {
+  if (!entry) return
+  const parentName = entry.split('/')[0]
+  const fileName = path.basename(entry, '.md')
+  const index = entries.findIndex(_ => _.name === parentName)
+  if (index < 0) {
+    entries.push({
+      name: parentName,
+      children: [fileName]
+    })
+    return
+  }
+  entries[index].children.push(fileName)
+})
+stream.once('error', console.log)
+stream.once('end', () => {
+  const content = entries.reduce((pre, {name, children}) => {
+    pre += `
+- ${name}
+${children.map(title => `  - [${title}](${linkPrefix}${name}/${title}.md)`).join('\n')}
+`
+    return pre
+  }, template)
+  fs.writeFileSync('README.md', content)
+})
